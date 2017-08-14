0e3c61859dc79eb9483fc8216bbb4a76895ad99e
p00s <pin9sb@gmail.com>
2017-08-14 19:05:02 +0800

    init
    init

---
+++ src/lib.rs
@@ -0,0 +1,132 @@
+//! minihttp is a simple and lightweight http client lib for rust.
+//! only provide basic http client feature, more like python request.
+//! minihttp's aim is simple, easy to use, less dependent, smaller binary.
+//! Enjoy your self...
+//!
+//! #Example
+//! ## send a get request
+//! ```
+//! use minihttp::request::Request;
+//!
+//! let mut http = Request::new("http://www.google.com").unwrap();
+//! let res = http.get().send().unwrap();
+//! println!("status code {}",res.status_code());
+//! println!("reason {}",res.reason());
+//! println!("body {}",res.text());
+//! for (k,v) in res.headers(){
+//!     println!("{}:{}",k,v);
+//! }
+//! ```
+//!
+//! ## send a post request
+//! ```
+//! use minihttp::request::Request;
+//!
+//! let mut http = Request::new("http://www.google.com").unwrap();
+//! let res = http.post().body_str("hello").send().unwrap();
+//! println!("status code {}",res.status_code());
+//! ```
+//!
+//! ## custom headers
+//! ```
+//! use std::collections::HashMap;
+//! use minihttp::request::Request;
+//!
+//! let mut http = Request::new("http://www.google.com").unwrap();
+//! let mut headers = HashMap::new();
+//! headers.insert("User-Agent","Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36");
+//! let res = http.headers(headers).send().unwrap();
+//! println!("status code {}",res.status_code());
+//! ```
+//!
+//! ## support https
+//! ```
+//! use minihttp::request::Request;
+//!
+//! let mut http = Request::new("https://www.google.com").unwrap();
+//! let res = http.get().send().unwrap();
+//! println!("status code {}",res.status_code());
+//! ```
+//!
+//! ## support proxy
+//! ```
+//! use minihttp::request::Request;
+//!
+//! let mut http = Request::new("http://www.google.com").unwrap();
+//! let res = http.proxy("http://127.0.0.1:1080").unwrap().get().send().unwrap();
+//! println!("status code {}",res.status_code());
+//! ```
+//!
+
+#![doc(html_root_url = "https://docs.rs/minihttp")]
+
+extern crate native_tls;
+extern crate minihttpse;
+extern crate miniurl;
+
+use miniurl::Url;
+use minihttpse::Response;
+use native_tls::TlsConnector;
+use native_tls::{Error,HandshakeError};
+
+use std::error;
+use std::io;
+use std::net::TcpStream;
+use std::fmt;
+
+///http request module
+pub mod request;
+
+///http basic error type
+#[derive(Debug)]
+pub enum HttpError {
+    Parse(&'static str),
+    Config(&'static str),
+    Proxy(&'static str),
+    IO(io::Error),
+    SSL(Error),
+    SSLHandshake(HandshakeError<TcpStream>),
+}
+
+
+impl From<io::Error> for HttpError {
+    fn from(err: io::Error) -> HttpError {
+        HttpError::IO(err)
+    }
+}
+
+impl From<minihttpse::HttpError> for HttpError {
+    fn from(err: minihttpse::HttpError) -> HttpError {
+        let minihttpse::HttpError::Parse(e) = err;
+        HttpError::Parse(e)
+    }
+}
+
+impl From<HandshakeError<TcpStream>> for HttpError {
+    fn from(err: HandshakeError<TcpStream>) -> HttpError {
+        HttpError::SSLHandshake(err)
+    }
+}
+
+impl From<Error> for HttpError {
+    fn from(err: Error) -> HttpError {
+        HttpError::SSL(err)
+    }
+}
+
+
+impl fmt::Display for HttpError {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        match *self {
+            HttpError::Parse(ref err) => write!(f, "Parse error: {}", err),
+            HttpError::Config(ref err) => write!(f, "Config error: {}", err),
+            HttpError::IO(ref err) => write!(f, "IO error: {}", err),
+            HttpError::Proxy(ref err) =>write!(f,"Proxy error : {}",err),
+            HttpError::SSL(ref err) => write!(f, "SSL error: {}", err),
+            HttpError::SSLHandshake(ref err) => write!(f, "SSL handshake error: {}", err),
+        }
+    }
+}
+
+
+
---
+++ src/request.rs
@@ -0,0 +1,481 @@
+use super::HttpError;
+use super::Url;
+use super::TlsConnector;
+use super::Response;
+
+
+use std::collections::HashMap;
+use std::io::prelude::*;
+use std::net::TcpStream;
+use std::time;
+
+///proxy info object.
+#[derive(Debug, Clone)]
+pub struct Proxy{
+    host: String,
+    port: u16,
+    scheme: String,
+    url: Url,
+}
+
+///http request object.
+#[derive(Debug, Clone)]
+pub struct Request{
+    host: String,
+    port: u16,
+    scheme: String,
+    method: String,
+    url: Url,
+    headers: HashMap<&'static str,&'static str>,
+    body: Option<Vec<u8>>,
+    timeout:u64,
+    proxy:Option<Proxy>,
+    verify:bool
+}
+
+impl Request{
+
+    ///return a Request object
+    /// # Example
+    /// ```
+    /// use minihttp::request::Request;
+    ///
+    /// let mut http = Request::new("https://www.google.com").unwrap();
+    /// ```
+    pub fn new(url:&str)->Result<Request,HttpError>{
+        let url:Url = Url::parse(url);
+
+        let host  = match url.host{
+            Some(ref h) => h.clone(),
+            None =>return Err(HttpError::Parse("url parse error"))
+        };
+        Ok(
+            Request{
+                host:host,
+                port:url.port,
+                scheme:url.scheme.clone(),
+                method:String::new(),
+                url:url,
+                headers:HashMap::new(),
+                body:None,
+                timeout:30,
+                proxy:None,
+                verify:true
+            }
+        )
+    }
+
+    ///set Request GET method
+    /// # Example
+    /// ```
+    /// use minihttp::request::Request;
+    ///
+    /// let mut http = Request::new("https://www.google.com").unwrap();
+    /// http.get();
+    /// ```
+    pub fn get(&mut self) -> &mut Self {
+        self.method = "GET".to_owned();
+        self
+    }
+
+    ///set Request POST method
+    /// # Example
+    /// ```
+    /// use minihttp::request::Request;
+    ///
+    /// let mut http = Request::new("https://www.google.com").unwrap();
+    /// http.post();
+    /// ```
+    pub fn post(&mut self) -> &mut Self {
+        self.method = "POST".to_owned();
+        self
+    }
+
+    ///set Request PUT method
+    /// # Example
+    /// ```
+    /// use minihttp::request::Request;
+    ///
+    /// let mut http = Request::new("https://www.google.com").unwrap();
+    /// http.put();
+    /// ```
+    pub fn put(&mut self) -> &mut Self {
+        self.method = "PUT".to_owned();
+        self
+    }
+
+    ///set Request HEAD method
+    /// # Example
+    /// ```
+    /// use minihttp::request::Request;
+    ///
+    /// let mut http = Request::new("https://www.google.com").unwrap();
+    /// http.head();
+    /// ```
+    pub fn head(&mut self) -> &mut Self {
+        self.method = "HEAD".to_owned();
+        self
+    }
+
+    ///set Request DELETE method
+    /// # Example
+    /// ```
+    /// use minihttp::request::Request;
+    ///
+    /// let mut http = Request::new("https://www.google.com").unwrap();
+    /// http.delete();
+    /// ```
+    pub fn delete(&mut self) -> &mut Self {
+        self.method = "DELETE".to_owned();
+        self
+    }
+
+    ///set Request OPTION method
+    /// # Example
+    /// ```
+    /// use minihttp::request::Request;
+    ///
+    /// let mut http = Request::new("https://www.google.com").unwrap();
+    /// http.option();
+    /// ```
+    pub fn option(&mut self) -> &mut Self {
+        self.method = "OPTION".to_owned();
+        self
+    }
+
+    ///set Request custom method
+    /// # Example
+    /// ```
+    /// use minihttp::request::Request;
+    ///
+    /// let mut http = Request::new("https://www.google.com").unwrap();
+    /// http.request("profile");
+    /// ```
+    pub fn request(&mut self, method: &str) -> &mut Self {
+        self.method = method.to_string();
+        self
+    }
+
+    ///set Request custom header
+    /// # Example
+    /// ```
+    /// use minihttp::request::Request;
+    /// use std::collections::HashMap;
+    ///
+    /// let mut http = Request::new("https://www.google.com").unwrap();
+    /// let mut headers = HashMap::new();
+    /// headers.insert("User-Agent","Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36");
+    /// http.headers(headers);
+    /// ```
+    pub fn headers(&mut self, data: HashMap<&'static str, &'static str>) -> &mut Self {
+        self.headers = data;
+        self
+    }
+
+    ///set Request body
+    /// # Example
+    /// ```
+    /// use minihttp::request::Request;
+    ///
+    /// let mut http = Request::new("https://www.google.com").unwrap();
+    /// let body = vec![0,1,2,3,4];
+    /// http.body(body);
+    /// ```
+    pub fn body(&mut self, data:Vec<u8>) -> &mut Self {
+        self.body = Some(data);
+        self
+    }
+
+    ///set Request string body
+    /// # Example
+    /// ```
+    /// use minihttp::request::Request;
+    ///
+    /// let mut http = Request::new("https://www.google.com").unwrap();
+    /// let body = "hello";
+    /// http.body_str(body);
+    /// ```
+    pub fn body_str(&mut self,data:&str) ->&mut Self{
+        let body = data.as_bytes().to_owned();
+        self.body = Some(body);
+        self
+    }
+
+    ///set Request read/write timeout(sec)
+    /// # Example
+    /// ```
+    /// use minihttp::request::Request;
+    ///
+    /// let mut http = Request::new("https://www.google.com").unwrap();
+    /// http.timeout(10);
+    /// ```
+    pub fn timeout(&mut self,time:u64) -> &mut Self{
+        self.timeout = time;
+        self
+    }
+
+    ///set https request  if verify the certificate(default true)
+    /// # Example
+    /// ```
+    /// use minihttp::request::Request;
+    ///
+    /// let mut http = Request::new("https://www.google.com").unwrap();
+    /// http.verify(false);
+    /// ```
+    pub fn verify(&mut self,verify:bool) -> Result<&mut Self,HttpError>{
+        if self.scheme == "https".to_owned(){
+            self.verify = verify;
+        }else {
+            return Err(HttpError::Config("Verify setting only for https"))
+        }
+        Ok(self)
+    }
+
+    ///set proxy info
+    /// # Example
+    /// ```
+    /// use minihttp::request::Request;
+    ///
+    /// let mut http = Request::new("https://www.google.com").unwrap();
+    /// http.proxy("https://127.0.0.1:1080");
+    /// ```
+    pub fn proxy(&mut self,proxy:&str) -> Result<&mut Self,HttpError>{
+        let url:Url = Url::parse(proxy);
+        if self.scheme == "https".to_owned() && url.scheme == "http".to_owned(){
+            return return Err(HttpError::Proxy("Http proxy can only use http scheme."))
+        }
+
+        let host  = match url.host{
+            Some(ref h) => h.clone(),
+            None =>return Err(HttpError::Parse("url parse error"))
+        };
+
+        let proxy = Proxy{
+            host:host,
+            port:url.port,
+            scheme:url.scheme.clone(),
+            url:url
+        };
+        self.proxy = Some(proxy);
+        Ok(self)
+    }
+
+    ///send https request
+    /// # Example
+    /// ```
+    /// use minihttp::request::Request;
+    ///
+    /// let mut http = Request::new("https://www.google.com").unwrap();
+    /// http.get().send();
+    /// ```
+    pub fn send(&mut self) -> Result<Response, HttpError> {
+        if let Some(ref proxy) = self.proxy{
+            if proxy.scheme == "http".to_owned(){
+                let header = self.build_header();
+                let addr = format!("{}:{}", proxy.host, proxy.port);
+                let mut stream = TcpStream::connect(addr)?;
+                stream.set_read_timeout(Some(time::Duration::from_secs(self.timeout)))?;
+                stream.set_write_timeout(Some(time::Duration::from_secs(self.timeout)))?;
+                stream.write(header.as_bytes())?;
+                if let Some(ref body) = self.body{
+                    stream.write(body.as_slice())?;
+                }
+                stream.flush()?;
+                let mut res :Vec<u8>= Vec::new();
+                stream.read_to_end(&mut res)?;
+                let back = Response::new(res)?;
+                Ok(back)
+            }else {
+
+                //CONNECT proxy.google.com:443 HTTP/1.1
+                //Host: www.google.com:443
+                //Proxy-Connection: keep-alive
+                let mut connect_header = String::new();
+                connect_header.push_str(&format!("CONNECT {}:{}\r\n",self.host,self.port));
+                connect_header.push_str(&format!("Host: {}:{}\r\n",self.host,self.port));
+                connect_header.push_str("\r\n");
+                let addr = format!("{}:{}", proxy.host, proxy.port);
+                let mut stream = TcpStream::connect(addr)?;
+                stream.set_read_timeout(Some(time::Duration::from_secs(self.timeout)))?;
+                stream.set_write_timeout(Some(time::Duration::from_secs(self.timeout)))?;
+                stream.write(connect_header.as_bytes())?;
+                stream.flush()?;
+
+                //HTTP/1.1 200 Connection Established
+                let mut res = String::new();
+                stream.read_to_string(&mut res)?;
+                if !res.contains("Connection Established"){
+                    return Err(HttpError::Proxy("Proxy server response error."));
+                }
+
+                if self.scheme == "http".to_owned(){
+                    let header = self.build_header();
+                    stream.write(header.as_bytes())?;
+                    if let Some(ref body) = self.body{
+                        stream.write(body.as_slice())?;
+                    }
+                    stream.flush()?;
+                    let mut res :Vec<u8>= Vec::new();
+                    stream.read_to_end(&mut res)?;
+                    let back = Response::new(res)?;
+                    Ok(back)
+
+                }else {
+                    let connector = TlsConnector::builder()?.build()?;
+                    let mut ssl_stream;
+                    if self.verify{
+                        ssl_stream = connector.connect(&&self.host, stream)?;
+                    }else {
+                        ssl_stream = connector.danger_connect_without_providing_domain_for_certificate_verification_and_server_name_indication(stream)?;
+                    }
+                    let header = self.build_header();
+                    ssl_stream.write(header.as_bytes())?;
+                    if let Some(ref body) = self.body{
+                        ssl_stream.write(body.as_slice())?;
+                    }
+                    ssl_stream.flush()?;
+                    let mut res :Vec<u8>= Vec::new();
+                    ssl_stream.read_to_end(&mut res)?;
+                    let back = Response::new(res)?;
+                    Ok(back)
+                }
+
+            }
+
+        }else {
+            if self.scheme =="http".to_owned(){
+                let header = self.build_header();
+                let addr = format!("{}:{}", self.host, self.port);
+                let mut stream = TcpStream::connect(addr)?;
+                stream.set_read_timeout(Some(time::Duration::from_secs(self.timeout)))?;
+                stream.set_write_timeout(Some(time::Duration::from_secs(self.timeout)))?;
+                stream.write(header.as_bytes())?;
+                if let Some(ref body) = self.body{
+                    stream.write(body.as_slice())?;
+                }
+                stream.flush()?;
+                let mut res :Vec<u8>= Vec::new();
+                stream.read_to_end(&mut res)?;
+                let back = Response::new(res)?;
+                Ok(back)
+            }else {
+                let header = self.build_header();
+                let addr = format!("{}:{}", self.host, self.port);
+                let mut stream = TcpStream::connect(addr)?;
+                stream.set_read_timeout(Some(time::Duration::from_secs(self.timeout)))?;
+                stream.set_write_timeout(Some(time::Duration::from_secs(self.timeout)))?;
+                let connector = TlsConnector::builder()?.build()?;
+                let mut ssl_stream;
+                if self.verify{
+                        ssl_stream = connector.connect(&&self.host, stream)?;
+                }else {
+                        ssl_stream = connector.danger_connect_without_providing_domain_for_certificate_verification_and_server_name_indication(stream)?;
+                }
+                let header = self.build_header();
+                ssl_stream.write(header.as_bytes())?;
+                if let Some(ref body) = self.body{
+                    ssl_stream.write(body.as_slice())?;
+                }
+                ssl_stream.flush()?;
+
+                let mut res :Vec<u8>= Vec::new();
+                ssl_stream.read_to_end(&mut res)?;
+                let back = Response::new(res)?;
+                Ok(back)
+            }
+        }
+    }
+
+    //build http request headers
+    fn build_header(&self) ->String{
+        if let  Some(ref proxy) = self.proxy{
+            if proxy.scheme == "http".to_owned(){
+                let mut headers = String::new();
+                headers.push_str(&format!("{} {} HTTP/1.1\r\n",self.method,self.url.as_string()));
+                headers.push_str(&format!("Host: {}:{}\r\n",self.host,self.port));
+                headers.push_str(&format!("Connection: Close\r\n"));
+                if let Some(ref body) = self.body{
+                    headers.push_str(&format!("Content-Length: {}\r\n",body.len()));
+                }
+                for (i,k) in &self.headers{
+                    headers.push_str(&format!("{}: {}\r\n",i,k));
+                }
+                headers.push_str("\r\n");
+                headers
+            }else {
+                let mut headers = String::new();
+                headers.push_str(&format!("{} {} HTTP/1.1\r\n",self.method,self.url.request_string()));
+                headers.push_str(&format!("Host: {}:{}\r\n",self.host,self.port));
+                headers.push_str(&format!("Connection: Close\r\n"));
+                if let Some(ref body) = self.body{
+                    headers.push_str(&format!("Content-Length: {}\r\n",body.len()));
+                }
+                for (i,k) in &self.headers{
+                    headers.push_str(&format!("{}: {}\r\n",i,k));
+                }
+                headers.push_str("\r\n");
+                headers
+            }
+        }else {
+                let mut headers = String::new();
+                headers.push_str(&format!("{} {} HTTP/1.1\r\n",self.method,self.url.request_string()));
+                headers.push_str(&format!("Host: {}:{}\r\n",self.host,self.port));
+                headers.push_str(&format!("Connection: Close\r\n"));
+                if let Some(ref body) = self.body{
+                    headers.push_str(&format!("Content-Length: {}\r\n",body.len()));
+                }
+                for (i,k) in &self.headers{
+                    headers.push_str(&format!("{}: {}\r\n",i,k));
+                }
+                headers.push_str("\r\n");
+                headers
+        }
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    #[test]
+    fn http_get() {
+        let mut http = Request::new("http://docs.rs/").unwrap();
+        println!("{}",http.get().send().unwrap().status_code())
+    }
+
+    #[test]
+    fn https_get() {
+        let mut http = Request::new("https://docs.rs/").unwrap();
+        println!("{}",http.verify(false).unwrap().get().send().unwrap().status_code())
+    }
+
+    #[test]
+    fn http_post() {
+        let mut http = Request::new("http://docs.rs/").unwrap();
+        println!("{}",http.post().body_str("username=bob").send().unwrap().status_code())
+    }
+
+    #[test]
+    fn http_get_set_header() {
+        let mut http = Request::new("http://docs.rs/").unwrap();
+        let mut headers = HashMap::new();
+        headers.insert("Content-Type","text/html; charset=utf-8");
+        println!("{}",http.headers(headers).get().send().unwrap().status_code())
+    }
+
+    #[test]
+    fn http_get_back_header() {
+        let mut http = Request::new("https://docs.rs/").unwrap();
+        let headers = http.get().send().unwrap().headers();
+        for (k,v) in headers{
+            println!("{}:{}",k,v);
+        }
+    }
+
+    #[test]
+    fn http_proxy() {
+        let mut http = Request::new("http://docs.rs/").unwrap();
+        let res = http.proxy("http://127.0.0.1:1080").unwrap().get().send().unwrap();
+        println!("{}",res.status_code());
+    }
+
+}
